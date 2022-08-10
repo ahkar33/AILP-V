d58a08b09298e0b51e411abfcb42f07a22c910a0
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-10 14:25:29 +0630

    initial commit

---
+++ src/main/java/com/ace/ailpv/AilpVApplication.java
@@ -0,0 +1,13 @@
+package com.ace.ailpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilpVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilpVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace/ailpv/AilpVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.ailpv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilpVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
