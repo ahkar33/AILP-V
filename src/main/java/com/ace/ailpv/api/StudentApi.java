32d8e990dcab07ba4d810e4e4f62b2a4e7fd73b4
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-15 01:39:03 +0630

    added student edit and teacher edit

--- src/main/java/com/ace/ailpv/api/StudentApi.java
+++ src/main/java/com/ace/ailpv/api/StudentApi.java
@@ -34,5 +34,5 @@ public class StudentApi {
             userService.addUser(student);
         }
     }
-
+    
 }
--- src/main/java/com/ace/ailpv/entity/Comment.java
+++ src/main/java/com/ace/ailpv/entity/Comment.java
@@ -1,39 +1,39 @@
-package com.ace.ailpv.entity;
+// package com.ace.ailpv.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+// import javax.persistence.CascadeType;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+// import lombok.ToString;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-public class Comment {
+// @Entity
+// @Getter
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @ToString
+// public class Comment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    private String commentBody;
+//     private String commentBody;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "video_id")
-    private Video commentVideo;
+//     @ManyToOne(cascade = CascadeType.ALL)
+//     @JoinColumn(name = "video_id")
+//     private Video commentVideo;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "student_id")
-    private Student commentStudent;
+//     @ManyToOne(cascade = CascadeType.ALL)
+//     @JoinColumn(name = "student_id")
+//     private Student commentStudent;
 
-}
+// }
