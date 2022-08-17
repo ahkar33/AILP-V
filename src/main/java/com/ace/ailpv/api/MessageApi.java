c90eea283e01688a8100809ca9eee05b12f69c3f
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-18 01:49:33 +0630

    added MessageApi

---
+++ src/main/java/com/ace/ailpv/api/MessageApi.java
@@ -0,0 +1,50 @@
+package com.ace.ailpv.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ace.ailpv.entity.Batch;
+import com.ace.ailpv.entity.Message;
+import com.ace.ailpv.entity.User;
+import com.ace.ailpv.service.BatchService;
+import com.ace.ailpv.service.MessageService;
+import com.ace.ailpv.service.UserService;
+
+@RestController
+@RequestMapping("/api/message")
+@CrossOrigin(origins = "*")
+public class MessageApi {
+
+    @Autowired 
+    private MessageService messageService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BatchService batchService;
+
+    @PostMapping("/addMessage")    
+    public void addMessage(@RequestBody Message message) {
+        Batch batch = batchService.getBatchById(Long.parseLong(message.getBatchId()));
+        User user = userService.getUserById(message.getUserId());
+        message.setMessageBatch(batch);
+        message.setMessageUser(user);
+        messageService.addMessage(message);
+    }
+
+    @GetMapping("/getMessagesByBatchId/{id}")
+    public List<Message> getAllMessages(@PathVariable("id") String id) {
+        long batchId = Long.parseLong(id); 
+        return messageService.getMessageListByBatchId(batchId);
+    }
+
+}
