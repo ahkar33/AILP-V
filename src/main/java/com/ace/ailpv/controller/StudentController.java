7c7bb3a1dfff00330c18fceaf71cb09dda613d11
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-19 16:59:37 +0630

    fixed message datetime bug

--- src/main/java/com/ace/ailpv/controller/StudentController.java
+++ src/main/java/com/ace/ailpv/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.ace.ailpv.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ace.ailpv.entity.User;
 
 @Controller
 @RequestMapping("/student")
@@ -14,7 +19,14 @@ public class StudentController {
     }
 
     @GetMapping("/student-public-chat")
-    public String setupStudentPublicChat() {
+    public String setupStudentPublicChat(HttpSession session, ModelMap model) {
+        User userInfo = (User) session.getAttribute("userInfo");
+        userInfo.setBatchId(userInfo.getBatchList().iterator().next().getId().toString());
+        userInfo.setBatchName(userInfo.getBatchList().iterator().next().getName().toLowerCase());
+        model.addAttribute("userId", userInfo.getId());
+        model.addAttribute("username", userInfo.getName());
+        model.addAttribute("batchId", userInfo.getBatchId());
+        model.addAttribute("batchName", userInfo.getBatchName());
         return "/student/STU-PBC-07";
     }
 
