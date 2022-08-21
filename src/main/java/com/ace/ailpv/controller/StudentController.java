0cbb408f8b3cba3f8e39d67f9e83533b42dd726b
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-21 16:39:26 +0630

    added student get resource

--- src/main/java/com/ace/ailpv/controller/StudentController.java
+++ src/main/java/com/ace/ailpv/controller/StudentController.java
@@ -1,17 +1,25 @@
 package com.ace.ailpv.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ace.ailpv.entity.BatchHasResource;
 import com.ace.ailpv.entity.User;
+import com.ace.ailpv.service.BatchHasResourceService;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private BatchHasResourceService batchHasResourceService;
 
     @GetMapping("/student-home")
     public String showStudentHomePage() {
@@ -28,6 +36,21 @@ public class StudentController {
         model.addAttribute("batchId", userInfo.getBatchId());
         model.addAttribute("batchName", userInfo.getBatchName());
         return "/student/STU-PBC-07";
+    }
+
+    @GetMapping("/getResources")
+    public String getResources(ModelMap model, HttpSession session) {
+        User studentInfo = (User) session.getAttribute("userInfo");
+        Long studentBatchId = studentInfo.getBatchList().iterator().next().getId();
+        List<BatchHasResource> batchHasResourceList = batchHasResourceService
+                .getAllBatchHasResourcesByBatchId(studentBatchId);
+        // for(BatchHasResource batchHasResource: BatchHasResourceList) {
+        //     System.out.println(batchHasResource.getResource().getName());
+        //     System.out.println(batchHasResource.getSchedule());
+        //     System.out.println("----------------------------------------");
+        // }
+        model.addAttribute("batchHasResourceList", batchHasResourceList);
+        return "/student/STU-REC-09";
     }
 
 }
