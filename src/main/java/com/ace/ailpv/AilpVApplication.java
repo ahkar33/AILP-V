a19ebfdf2376a218e649c607e6fc668493664d91
Sawhelium <sawwailwin.2022@gmail.com>
2022-08-25 12:28:41 +0630

    commited

--- src/main/java/com/ace/ailpv/AilpVApplication.java
+++ src/main/java/com/ace/ailpv/AilpVApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AilpVApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AilpVApplication.class, args);
 	}
