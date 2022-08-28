b69c62489bdc3d174ba5cba512967c630366b991
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-28 23:29:12 +0630

    added student video

--- src/main/java/com/ace/ailpv/controller/StudentController.java
+++ src/main/java/com/ace/ailpv/controller/StudentController.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ace.ailpv.entity.Batch;
 import com.ace.ailpv.entity.BatchHasResource;
+import com.ace.ailpv.entity.BatchHasVideo;
 import com.ace.ailpv.entity.User;
 import com.ace.ailpv.service.BatchHasResourceService;
+import com.ace.ailpv.service.BatchHasVideoService;
+import com.ace.ailpv.service.BatchService;
 import com.ace.ailpv.service.UserService;
 
 @Controller
@@ -23,11 +28,17 @@ public class StudentController {
     private BatchHasResourceService batchHasResourceService;
 
     @Autowired
+    private BatchHasVideoService batchHasVideoService;
+
+    @Autowired
     private UserService usersService;
+
+    @Autowired
+    private BatchService batchService;
 
     @GetMapping("/student-home")
     public String showStudentHomePage(HttpSession session, ModelMap model) {
-        String studentId =  (String) session.getAttribute("uid");
+        String studentId = (String) session.getAttribute("uid");
         User userInfo = usersService.getUserById(studentId);
         userInfo.setBatchId(userInfo.getBatchList().iterator().next().getId().toString());
         model.addAttribute("userId", userInfo.getId());
@@ -37,7 +48,7 @@ public class StudentController {
 
     @GetMapping("/student-public-chat")
     public String setupStudentPublicChat(HttpSession session, ModelMap model) {
-        String studentId =  (String) session.getAttribute("uid");
+        String studentId = (String) session.getAttribute("uid");
         User userInfo = usersService.getUserById(studentId);
         userInfo.setBatchId(userInfo.getBatchList().iterator().next().getId().toString());
         userInfo.setBatchName(userInfo.getBatchList().iterator().next().getName());
@@ -50,13 +61,40 @@ public class StudentController {
 
     @GetMapping("/getResources")
     public String getResources(ModelMap model, HttpSession session) {
-        String studentId =  (String) session.getAttribute("uid");
+        String studentId = (String) session.getAttribute("uid");
         User studentInfo = usersService.getUserById(studentId);
         Long studentBatchId = studentInfo.getBatchList().iterator().next().getId();
         List<BatchHasResource> batchHasResourceList = batchHasResourceService
                 .getAllBatchHasResourcesByBatchId(studentBatchId);
         model.addAttribute("batchHasResourceList", batchHasResourceList);
         return "/student/STU-REC-09";
+    }
+
+    @GetMapping("/getVideos")
+    public String getVideos(ModelMap model, HttpSession session) {
+        String studentId = (String) session.getAttribute("uid");
+        User studentInfo = usersService.getUserById(studentId);
+        Long studentBatchId = studentInfo.getBatchList().iterator().next().getId();
+        Batch studentBatch = batchService.getBatchById(studentBatchId);
+        List<BatchHasVideo> batchHasVideoList = batchHasVideoService
+                .getAllBatchHasVideoByBatchId(studentBatchId);
+        model.addAttribute("courseName", studentBatch.getBatchCourse().getName());
+        model.addAttribute("batchHasVideoList", batchHasVideoList);
+        return "/student/STU-VID-06";
+    }
+
+    @GetMapping("/showClickedVideo/{courseName}/{videoName}")
+    public String showClickedVideo(HttpSession session, ModelMap model, @PathVariable("videoName") String videoName,
+            @PathVariable("courseName") String courseName) {
+        String studentId = (String) session.getAttribute("uid");
+        User studentInfo = usersService.getUserById(studentId);
+        Long studentBatchId = studentInfo.getBatchList().iterator().next().getId();
+        List<BatchHasVideo> batchHasVideoList = batchHasVideoService
+                .getAllBatchHasVideoByBatchId(studentBatchId);
+        model.addAttribute("videoName", videoName);
+        model.addAttribute("courseName", courseName);
+        model.addAttribute("batchHasVideoList", batchHasVideoList);
+        return "/student/STU-VID-06";
     }
 
 }
