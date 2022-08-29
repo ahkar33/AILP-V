e56d70bb2a3d0e24de609a28b2635f76050f0ecd
Sawhelium <sawwailwin.2022@gmail.com>
2022-08-29 11:28:22 +0630

    nth

--- src/main/java/com/ace/ailpv/AilpVApplication.java
+++ src/main/java/com/ace/ailpv/AilpVApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SecretConfigProperties.class)
+@EnableConfigurationProperties (SecretConfigProperties.class)
 public class AilpVApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AilpVApplication.class, args);
