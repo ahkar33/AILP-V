36a336c3698605746582fb9884f151fb7912d81d
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-08-31 10:08:48 +0630

    added teacher names in student video and added some validation

--- src/main/java/com/ace/ailpv/repository/VideoRepository.java
+++ src/main/java/com/ace/ailpv/repository/VideoRepository.java
@@ -3,6 +3,7 @@ package com.ace.ailpv.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ace.ailpv.entity.Video;
 
@@ -11,5 +12,8 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
     Boolean existsByNameAndVideoCourse_id(String name, Long id);
 
     List<Video> findByVideoCourse_id(Long id);
+
+    @Query(value = "SELECT * FROM video ORDER BY id ASC LIMIT 1", nativeQuery = true)
+    Video findFirstVideo();
 
 }
